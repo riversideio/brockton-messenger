5af376d40fd8b7b24d7cd51bc02fc70a8d43ee1f
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-02-01 11:10:46 -0800

    adding in auth header for produciton and also sending back acurrate status codes

--- index.js
+++ index.js
@@ -3,45 +3,50 @@ require('dotenv').load();
 var connect = require('connect'),
 	http = require('http'),
 	api = process.env.API || 'http://localhost:3000/',
-	router = require('./libs/router')
-		( require('./libs/routes') ),
 	Serial = require('./libs/serial'),
-	qs = require('querystring'),
-	port = process.env.PORT || 3000,
+	env = process.env,
+	port = env.PORT || 3000,
 	createKeys = process.argv[2] === 'create',
-	io = require('./libs/sdk')( process.env.API ),
-	serial = new Serial( process.env.SERIALPORT, 
-		{
-			auth : process.env.SERIALAUTH
-		}
-	),
-	home =  new ( require('./libs/home'))
-		( process.env.DBDIR, serial, io );
-	app = connect( );
+	io = require('./libs/sdk')( env.API ),
+	app = connect( ),
+	router = require('./libs/router')( require('./libs/routes') ),
+	serial = new Serial( env.SERIALPORT, {
+		auth : env.SERIALAUTH
+	}),
+	home =  new ( require('./libs/home') )( env.DBDIR, serial, io );
 
-app.use(function( req, res, next ){
-	req.payload = {};
-	// simple data parser
-	req.on('data', function(chunk){
-		req.payload= qs.parse(chunk);
-	}).on('end', next )
-	req.home = home;
-});
-app.use(router);
+
+if ( env.NODE_ENV === 'production' ) {
+	app.use(function( req, res, next ) {
+		var auth = req.headers.authorization;
+		console.log( auth )
+		if ( auth && auth === env.SECRETAUTH ) {
+			return next();
+		}
+		res.statusCode = 401;
+		res.end('Not Authorized');
+	});
+}
+
+app
+	.use(connect.bodyParser())
+	.use(connect.query())
+	.use(function( req, res, next ){
+		req.home = home;
+		next();
+	})
+	.use(router);
 
 serial.on('user:granted', function ( user ) {
-	console.log( user, 'granted access ');
 	delete user.key;
 	var opts = {
 		rfid : user.id
-	}
+	};
 	// send a checkin
 	io.users.checkin( opts, function( err, res ){
 		console.log( res.status );
 	})
 });
-var emptyUsers = {},
-	emptyCount = 0;
 
 serial.on('auth', function( isGood ) {
 	if ( !isGood ) return;	
@@ -49,38 +54,30 @@ serial.on('auth', function( isGood ) {
 });
 
 // this is a quick way to associate muliple tags in a row
-function associateKey ( key, user ) {
-	console.log("Associating user id " + user.id + " with " + key );
-	console.log(" in 10 sec, end node proccess to stop");
-	setTimeout(function(){
-		user.permission = '5';
-		user.key = key;
-		serial.updateUser( user, function( err, res ) {
-			if ( err ) return console.log( err );
-			delete emptyUsers[ user.id ];
-		});
-	}, 10000 );
-}
+// function associateKey ( key, user ) {
+// 	console.log("Associating user id " + user.id + " with " + key );
+// 	console.log(" in 10 sec, end node proccess to stop");
+// 	setTimeout(function(){
+// 		user.permission = '5';
+// 		user.key = key;
+// 		serial.updateUser( user, function( err, res ) {
+// 			if ( err ) return console.log( err );
+// 			delete emptyUsers[ user.id ];
+// 		});
+// 	}, 10000 );
+// }
 
 
 serial.on('user:denied', function ( token ) {
 	console.log( token, 'denied' );
-	if ( createKeys ) {
-		for ( var key in emptyUsers ) {
-			var user = emptyUsers[ key ];
-			return associateKey( token, user );
-		}
-	}
+	// TODO switch this to use home.getUsers({empty:true});
+	// if ( createKeys ) {
+	// 	for ( var key in emptyUsers ) {
+	// 		var user = emptyUsers[ key ];
+	// 		return associateKey( token, user );
+	// 	}
+	// }
 });
-
-// home.updateUser( '9', { hello : 'world' }, function( err, res) {
-// 	console.log( arguments )
-// })
-
-/*io.users.all(function( err, res ){
-	console.log( arguments );
-})*/
-
 
 http.createServer( app )
 	.listen( port );
--- libs/routes.js
+++ libs/routes.js
@@ -3,46 +3,67 @@ module.exports = {
 	"GET /" : function ( req, res ) {
 		var file = '/index.html';
 		fs.readFile( __dirname + '/..' + file, function ( err, data ) {
-			if( err ) return res.end( err.message )
+			if( err ) {
+				res.statusCode = 500;
+				return res.end( err.message )
+			}
 			res.end( data );
 		})
 	},
 	"GET /users.json" : function ( req, res ) {
 		req.home.getUsers( function( err, users ){
-			if( err ) return res.end( err.message );
+			if( err ) {
+				res.statusCode = 500;
+				return res.end( err.message )
+			}
 			res.end( JSON.stringify( users ) );
 		})
 	},
 	"GET /users/{id}.json" : function ( req, res ) {
 		var params = req.params;
 		req.home.getUser( params.id, function( err, user ){
-			if( err ) return res.end( err.message );
+			if( err ) {
+				res.statusCode = 500;
+				return res.end( err.message )
+			}
 			res.end( JSON.stringify(user) );
 		})
 	},
 	"PUT /users/{id}.json" : function ( req, res ) {
 		var params = req.params,
-			payload = req.payload;
+			payload = req.body;
 
 		req.home.updateUser( params.id, payload, function( err, user ){
-			if( err ) return res.end( err.message );
+			if( err ) {
+				res.statusCode = 500;
+				return res.end( err.message )
+			}
 			res.end( JSON.stringify( user ) );
 		})
 	},
 	"POST /users/{id}.json" : function ( req, res ) {
 		var params = req.params,
-			payload = req.payload,
+			payload = req.body,
 			user;
 
 		req.home.getUsers( { empty : true }, function ( err, users ) {
-			res.end(JSON.stringify(users))
-			if ( !user.length ) return res.end("no available slots to write user");
+			if( err ) {
+				res.statusCode = 500;
+				return res.end( err.message )
+			}
+			if ( !user.length ) {
+				res.status = 507;
+				return res.end("no available slots to write user");
+			}
 			user = users[0];
 			// generate key
 			// check unique
 			payload.key = '1234';
 			req.home.updateUser( user.id, payload, function ( err, user ) {
-				if ( err ) return res.end(err.message);
+				if( err ) {
+					res.statusCode = 500;
+					return res.end( err.message )
+				}
 				res.end( JSON.stringify( user ));
 			});
 		});
